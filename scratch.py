6192804e88e0996b1d05efc9619340a62b10fcb4
Nevedya Gwalani <115075447+NiviDevs@users.noreply.github.com>
2026-01-11 03:41:27 +0530

    extracted raw html

--- scratch.py
+++ scratch.py
@@ -3,3 +3,10 @@ import requests
 r = requests.get("https://nptel.ac.in/courses")
 print(r.status_code)
 print(r.text[:500])
+
+with open(
+    "downloadable_html.html",
+    "w",
+) as f:
+    f.write(r.text)
+# raw html i shal not push because its like 30k lines dawg
